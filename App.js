43cf4f51ef0100bcc51697916e5d929387687cb3
Erwan Riou <rr.1.riou@gmail.com>
2018-06-18 17:04:28 +0200

    starting the react-native-app and adding utilities files

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>THIS IS A STUPID APP THAT DO KAPSLOCK THINGS....</Text>
+        <Text>IS IT?</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
