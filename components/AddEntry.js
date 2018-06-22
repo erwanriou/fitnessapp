ef08493d749f1e1fcd7b6d3f3f7b1c421049a6c2
Erwan Riou <rr.1.riou@gmail.com>
2018-06-22 12:32:18 +0200

    updating and creating touchable device

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
 
-import { getMetricMetaInfo } from '../utils/helpers'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import Stepper from './Stepper'
 import Slider from './Slider'
+import DateHeader from './DateHeader'
+
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}>
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
 
 export default class AddEntry extends Component {
 
@@ -42,11 +52,28 @@ export default class AddEntry extends Component {
     }))
   }
 
+  submit = () => {
+    const key = timeToString()
+    const entry = this.state
+    // Update Redux
+    this.setState(() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }))
+    //Navigate to home
+    //Save to 'DB'
+    //Clean local Notification
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
 
     return (
       <View>
+        <DateHeader date={(new Date()).toLocaleDateString()} />
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -70,6 +97,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     )
   }
