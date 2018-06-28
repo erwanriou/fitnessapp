371da5a0fc448317c3f16edd6848faa1eb90e5a7
Erwan Riou <rr.1.riou@gmail.com>
2018-06-28 17:17:24 +0200

    fix issue on action.entry function

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text } from 'react-native'
+import { View, TouchableOpacity, Text, Platform, StyleSheet } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { connect } from 'react-redux'
 
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
+import { white, purple } from '../utils/colors'
 import { submitEntry, removeEntry } from '../utils/api'
 import Stepper from './Stepper'
 import CreateSlider from './Slider'
@@ -11,11 +12,12 @@ import DateHeader from './DateHeader'
 import TextButton from './TextButton'
 import { addEntry } from '../actions'
 
-function SubmitBtn({ onPress }) {
+function SubmitBtn ({ onPress }) {
   return (
     <TouchableOpacity
+      style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.AndroidSubmitBtn}
       onPress={onPress}>
-      <Text>SUBMIT</Text>
+        <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
@@ -81,7 +83,7 @@ class AddEntry extends Component {
       [key]: getDailyReminderValue()
     }))
     // Route to home
-    removeEntry({ key })
+    removeEntry(key)
   }
 
   render() {
@@ -103,14 +105,14 @@ class AddEntry extends Component {
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={(new Date()).toLocaleDateString()} />
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === 'slider'
                 ? <CreateSlider
@@ -133,6 +135,43 @@ class AddEntry extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  androidSubmistBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+})
 
 function mapStateToProps (state) {
   const key = timeToString()
