ecd503186355f854a163a43e25e23b5bce319e9a
Erwan Riou <rr.1.riou@gmail.com>
2018-06-29 13:55:17 +0200

    adding calenday feature to the application

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 
 import reducer from './reducers'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 
 
 export default class App extends React.Component {
@@ -12,7 +13,8 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <View style={{height: 20}} />
+          <History />
         </View>
       </Provider>
     )
