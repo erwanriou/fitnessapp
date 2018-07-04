72680f9c356f0301f3724f2caed42364056dc2ca
Erwan Riou <rr.1.riou@gmail.com>
2018-07-04 16:51:12 +0200

    updating ask geolocation permission

--- components/Live.js
+++ components/Live.js
@@ -13,6 +13,7 @@ export default class Live extends Component {
     status: null,
     direction: '',
   }
+
   componentDidMount () {
     Permissions.getAsync(Permissions.LOCATION)
       .then(({ status }) => {
@@ -26,8 +27,16 @@ export default class Live extends Component {
         this.setState(() => ({ status: 'undetermined' }))
       })
   }
-  askPermission() {
 
+  askPermission() {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => console.warn('error asking Location permission', error))
   }
 
   setLocation = () => {
@@ -79,7 +88,7 @@ export default class Live extends Component {
         <View style={styles.container}>
           <View style={styles.directionContainer}>
             <Text style={styles.header}>You 're heading</Text>
-            <Text style={styles.direction}>North</Text>
+            <Text style={styles.direction}>{direction}</Text>
           </View>
           <View style={styles.metricContainer}>
             <View style={styles.metric}>
@@ -87,7 +96,7 @@ export default class Live extends Component {
                 Altitude
               </Text>
               <Text style={[styles.subHeader, {color: white}]}>
-                {200} Feet
+                {Math.round(coords.altitude * 3.2808)} Feet
               </Text>
             </View>
             <View style={styles.metric}>
@@ -95,7 +104,7 @@ export default class Live extends Component {
                 Speed
               </Text>
               <Text style={[styles.subHeader, {color: white}]}>
-                {300} MPH
+                {(coords.speed * 2.2369).toFixed(1)} MPH
               </Text>
             </View>
           </View>
